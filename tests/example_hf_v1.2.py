f041097f27b25e0b0efe3e1112b78b69f2d57bb3
Kethan Dosapati <142290804+DKethan@users.noreply.github.com>
2025-05-12 18:37:35 -0400

    Added some examples

--- tests/example_hf_v1.2.py
+++ tests/example_hf_v1.2.py
@@ -4,6 +4,9 @@ client = connect("huggingface_inference", "microsoft/Phi-3-mini-4k-instruct", ""
 
 # Get a joke from the model
 joke_response = client.chat("Tell me a joke.")
+print(joke_response)
+
+print("\n---\n")
 
 # Prepare a prompt for code generation
 user_prompt = "give me a code to create a simple calculator in python"
--- tests/example_openai_v1.1.py
+++ tests/example_openai_v1.1.py
@@ -6,16 +6,16 @@ client = connect(provider="openai", model="gpt-4o", api_key="")
 joke_response = client.chat("Tell me a joke.")
 print(joke_response)
 
-print("\n---\n")
-
-# Prepare a prompt for code generation
-user_prompt = "give me a code to create a simple calculator in python"
-
-chat_history = [
-    {"role": "system", "content": "You are a state-of-the-art Python developer. You need to answer the question."},
-    {"role": "user", "content": user_prompt}
-]
-
-# Get a response using chat history
-code_response = client.chat_with_history(chat_history)
-print(code_response)
+# print("\n---\n")
+#
+# # Prepare a prompt for code generation
+# user_prompt = "give me a code to create a simple calculator in python"
+#
+# chat_history = [
+#     {"role": "system", "content": "You are a state-of-the-art Python developer. You need to answer the question."},
+#     {"role": "user", "content": user_prompt}
+# ]
+#
+# # Get a response using chat history
+# code_response = client.chat_with_history(chat_history)
+# print(code_response)
